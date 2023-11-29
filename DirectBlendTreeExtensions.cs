ee708ef9a36ec00ce56e00e4271ad709932dde91
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2023-11-29 09:57:32 +0900

    修正

--- DirectBlendTreeExtensions.cs
+++ DirectBlendTreeExtensions.cs
@@ -8,11 +8,6 @@
             return tree;
         }
 
-        private static T AddTo<T>(this T tree, LogicNOTGateBlendTree blendTree) where T : IDirectBlendTreeItem
-        {
-            blendTree.Item = tree;
-            return tree;
-        }
 
         public static DirectBlendTree AddDirectBlendTree(this DirectBlendTree directBlendTree, string name = null) => new DirectBlendTree(directBlendTree.ParameterName) { Name = name }.AddTo(directBlendTree);
 
@@ -23,18 +18,6 @@
         public static LogicORGateBlendTree AddOrGate(this DirectBlendTree directBlendTree, string name = null) => new LogicORGateBlendTree() { Name = name }.AddTo(directBlendTree);
 
         public static LogicANDGateBlendTree AddAndGate(this DirectBlendTree directBlendTree, string name = null) => new LogicANDGateBlendTree() { Name = name }.AddTo(directBlendTree);
-
-        public static LogicNOTGateBlendTree AddNotGate(this DirectBlendTree directBlendTree, string name = null) => new LogicNOTGateBlendTree() { Name = name }.AddTo(directBlendTree);
-
-
-
-        public static ToggleBlendTree AddToggle(this LogicNOTGateBlendTree notGate, string name = null) => new ToggleBlendTree() { Name = name }.AddTo(notGate);
-
-        public static RadialPuppetBlendTree AddRadialPuppet(this LogicNOTGateBlendTree notGate, string name = null) => new RadialPuppetBlendTree() { Name = name }.AddTo(notGate);
-
-        public static LogicORGateBlendTree AddOrGate(this LogicNOTGateBlendTree notGate, string name = null) => new LogicORGateBlendTree() { Name = name }.AddTo(notGate);
-
-        public static LogicANDGateBlendTree AddAndGate(this LogicNOTGateBlendTree notGate, string name = null) => new LogicANDGateBlendTree() { Name = name }.AddTo(notGate);
 
     }
 }
